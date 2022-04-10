82a6b054401d4ceb4f54bb393c018bcc6d1a2e0b
Dayanna <dzuniga145@gmail.com>
2022-04-10 00:41:18 -0600

    Agregando Cambios

--- app/src/main/java/com/example/parcialii/Agregar.java
+++ app/src/main/java/com/example/parcialii/Agregar.java
@@ -1,14 +1,81 @@
 package com.example.parcialii;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Agregar extends AppCompatActivity {
+
+    private Button addCourseBtn;
+    private TextInputEditText courseNameEdt, courseDescEdt, coursePriceEdt, bestSuitedEdt, courseImgEdt, courseLinkEdt;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    private ProgressBar loadingPB;
+    private String courseID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
+
+        addCourseBtn = findViewById(R.id.idBtnAddCourse);
+        courseNameEdt = findViewById(R.id.idEdtCourseName);
+        courseDescEdt = findViewById(R.id.idEdtCourseDescription);
+        coursePriceEdt = findViewById(R.id.idEdtCoursePrice);
+        bestSuitedEdt = findViewById(R.id.idEdtSuitedFor);
+        courseImgEdt = findViewById(R.id.idEdtCourseImageLink);
+        courseLinkEdt = findViewById(R.id.idEdtCourseLink);
+        loadingPB = findViewById(R.id.idPBLoading);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        // on below line creating our database reference.
+        databaseReference = firebaseDatabase.getReference("Courses");
+        // adding click listener for our add course button.
+        addCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingPB.setVisibility(View.VISIBLE);
+                // getting data from our edit text.
+                String courseName = courseNameEdt.getText().toString();
+                String courseDesc = courseDescEdt.getText().toString();
+                String coursePrice = coursePriceEdt.getText().toString();
+                String bestSuited = bestSuitedEdt.getText().toString();
+                String courseImg = courseImgEdt.getText().toString();
+                String courseLink = courseLinkEdt.getText().toString();
+                courseID = courseName;
+                // on below line we are passing all data to our modal class.
+                ModalRV courseRVModal = new ModalRV(courseID, courseName, courseDesc, coursePrice, bestSuited, courseImg, courseLink);
+                // on below line we are calling a add value event
+                // to pass data to firebase database.
+                databaseReference.addValueEventListener(new ValueEventListener() {
+
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // on below line we are setting data in our firebase database.
+                        databaseReference.child(courseID).setValue(courseRVModal);
+                        // displaying a toast message.
+                        Toast.makeText(Agregar.this, "Course Added..", Toast.LENGTH_SHORT).show();
+                        // starting a main activity.
+                        startActivity(new Intent(Agregar.this, MainActivity.class));
+                    }
+
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        // displaying a failure message on below line.
+                        Toast.makeText(Agregar.this, "Fail to add Course..", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
